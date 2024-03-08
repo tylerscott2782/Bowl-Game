fb44f758619277e0ccd6a9e22c0a2a6850cd4270
tylerscott2782 <91347716+tylerscott2782@users.noreply.github.com>
2024-03-08 15:15:12 -0600

    added logic for creating accounts when submitting the create account form

--- bowlsite/bowlapp/views.py
+++ bowlsite/bowlapp/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
@@ -10,7 +11,19 @@ def login(request):
     return render(request, "bowlapp/login.html")
 
 def createaccount(request):
-    return render(request, "bowlapp/createaccount.html")
+        if request.method == "POST":
+            try:
+                email = request.POST["email"]
+                password = request.POST["password"]
+                retyped_password = request.POST["retyped_password"]
+                if password != retyped_password:
+                    return HttpResponse("password did not match retyped password")
+                User.objects.create_user("user", email, password)
+                return HttpResponseRedirect(reverse("bowlapp:login"))
+            except:
+                return HttpResponseRedirect(reverse("bowlapp:createaccount"))
+        else:
+            return render(request, "bowlapp/createaccount.html")
 
 def overview(request):
     return HttpResponse("overview")
