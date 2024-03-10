15c32fb526323f70ba740e71e1c2fb79efdbe073
tylerscott2782 <91347716+tylerscott2782@users.noreply.github.com>
2024-03-09 23:17:16 -0600

    add leaderboard model

--- bowlsite/bowlapp/models.py
+++ bowlsite/bowlapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
+class Leaderboard(models.Model):
+    name = models.CharField(max_length=200)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="+")
+    date_created = models.DateField(auto_now_add=True)
+    members = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.name
